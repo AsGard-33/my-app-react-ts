c7303e8f1f589ce9b9d6e87797dc06efa5572055
Akuji <akujil26@gmail.com>
2024-02-29 19:14:40 +0100

    lesson23

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import './App.css';
+import GlobalStyles from "styles/GlobalStyles";
 
 // import Lesson20 from './lessons/lesson20/Lesson20';
 // import Lesson21 from './lessons/lesson21/Lesson21';
 // import Lesson22 from './lessons/lesson22/Lesson22';
+import Lesson23 from "lessons/lesson23/Lesson23";
 
 // import Homework20 from './homeworks/homework20/Homework20';
 // import Homework21 from './homeworks/homework21/Homework21';
-import Homework22 from 'homeworks/homework22/Homework22';
+// import Homework22 from 'homeworks/homework22/Homework22';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <GlobalStyles />
       {/* <Lesson20/> */}
       {/* <Lesson21/> */}
       {/* <Lesson22/> */}
       {/* <Homework20/> */}
       {/* <Homework21/> */}
-      <Homework22/>
-    </div>
+      {/* <Homework22/> */}
+      <Lesson23 />
+    </>
   );
 }
 
